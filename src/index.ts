a058974afaa0f66e0aff83f1f796642a6840df93
hirohata <hirohatatro@gmail.com>
2024-02-10 10:31:54 -0800

    feat: changed the logging function.

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,27 @@
 type Level = "Log" | "Info" | "Warn" | "Error";
 
 export class Logger {
-	private _log(level: Level, ...message: any[]) {
+	private _log(level: Level, console: (...date: any[]) => void , ...message: any[]) {
 		if (message) {
-			console.log(`[${level}]`, ...message);
+			console(`[${level}]`, ...message);
 		} else {
-			console.log(`[${level}]`);
+			console(`[${level}]`);
 		}
 	}
 
 	log(...message: any[]) {
-		this._log("Log", ...message);
+		this._log("Log", console.log, ...message);
 	}
 
 	info(...message: any[]) {
-		this._log("Info", ...message);
+		this._log("Info", console.info, ...message);
 	}
 
 	warn(...message: any[]) {
-		this._log("Warn", ...message);
+		this._log("Warn", console.warn, ...message);
 	}
 
 	error(...message: any[]) {
-		this._log("Error", ...message);
+		this._log("Error", console.error, ...message);
 	}
 }
